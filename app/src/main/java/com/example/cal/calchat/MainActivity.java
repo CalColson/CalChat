9410543bd9b2cfda839c69253e019c5456137b5e
Cal Colson <cpcolson@yahoo.com>
2016-11-27 16:48:37 -0600

    finish remote config and entire tutorial

--- app/src/main/java/com/example/cal/calchat/MainActivity.java
+++ app/src/main/java/com/example/cal/calchat/MainActivity.java
@@ -18,12 +18,14 @@ package com.example.cal.calchat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -38,6 +40,8 @@ import android.widget.Toast;
 
 import com.example.cal.calchat.Utility.Constants;
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -45,10 +49,17 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
     public static final int RC_SIGN_IN = 1;
+    private static final int RC_PHOTO_PICKER = 2;
 
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
@@ -68,11 +80,12 @@ public class MainActivity extends AppCompatActivity {
 
     private String mUsername;
 
+    private StorageReference mPhotosRef;
     private DatabaseReference mMessagesRef;
     private ChildEventListener mMessagesListener;
-
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseRemoteConfig mRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +97,8 @@ public class MainActivity extends AppCompatActivity {
         mContext = this;
         mAuth = FirebaseAuth.getInstance();
         mMessagesRef = Constants.REF_MESSAGES;
+        mPhotosRef = Constants.REF_PHOTOS;
+        mRemoteConfig = FirebaseRemoteConfig.getInstance();
 
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -104,7 +119,12 @@ public class MainActivity extends AppCompatActivity {
         mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Fire an intent to show an image picker
+                String title = "Complete action using";
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent, title), RC_PHOTO_PICKER);
             }
         });
 
@@ -133,8 +153,6 @@ public class MainActivity extends AppCompatActivity {
         mSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Send messages on click
-
                 FriendlyMessage newMessage = new FriendlyMessage(mMessageEditText.getText().toString(),
                         mUsername, null);
                 mMessagesRef.push().setValue(newMessage);
@@ -168,6 +186,42 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG).build();
+        mRemoteConfig.setConfigSettings(configSettings);
+
+        Map<String, Object> defaultConfigMap = new HashMap<>();
+        defaultConfigMap.put(Constants.MAX_MSG_LENGTH_KEY, DEFAULT_MSG_LENGTH_LIMIT);
+        mRemoteConfig.setDefaults(defaultConfigMap);
+        fetchConfig();
+    }
+
+    private void fetchConfig() {
+        long cacheExpiration = 3600;
+        if (mRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
+            cacheExpiration = 0;
+        }
+
+        mRemoteConfig.fetch(cacheExpiration).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                mRemoteConfig.activateFetched();
+                applyRetrievedLengthLimit();
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                applyRetrievedLengthLimit();
+            }
+        });
+    }
+
+    private void applyRetrievedLengthLimit() {
+        Long max_msg_length = mRemoteConfig.getLong(Constants.MAX_MSG_LENGTH_KEY);
+
+        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(max_msg_length.intValue())});
+        Log.v(TAG, "I'm here! " + Constants.MAX_MSG_LENGTH_KEY + " = " + max_msg_length);
     }
 
     @Override
@@ -175,6 +229,22 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
             if (resultCode == RESULT_CANCELED) finish();
+        }
+
+        if (requestCode == RC_PHOTO_PICKER && resultCode == RESULT_OK) {
+            Uri selectedImageUri = data.getData();
+            StorageReference photoRef =
+                    mPhotosRef.child(selectedImageUri.getLastPathSegment());
+            UploadTask uploadTask = photoRef.putFile(selectedImageUri);
+            uploadTask.addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    FriendlyMessage photoMessage = new FriendlyMessage(null, mUsername, downloadUrl.toString());
+
+                    mMessagesRef.push().setValue(photoMessage);
+                }
+            });
         }
     }
 
@@ -196,7 +266,6 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onChildRemoved(DataSnapshot dataSnapshot) {
-
                 }
 
                 @Override
--- app/src/main/java/com/example/cal/calchat/Utility/Constants.java
+++ app/src/main/java/com/example/cal/calchat/Utility/Constants.java
@@ -2,12 +2,21 @@ package com.example.cal.calchat.Utility;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class Constants {
 
     //Firebase Constants
     public static final String KEY_MESSAGES = "messages";
+    public static final String KEY_PHOTOS = "chat_photos";
 
-    public static final DatabaseReference REF_ROOT = FirebaseDatabase.getInstance().getReference();
-    public static final DatabaseReference REF_MESSAGES = REF_ROOT.child(KEY_MESSAGES);
+    public static DatabaseReference REF_ROOT = FirebaseDatabase.getInstance().getReference();
+    public static DatabaseReference REF_MESSAGES = REF_ROOT.child(KEY_MESSAGES);
+
+    public static StorageReference REF_STORAGE_ROOT = FirebaseStorage.getInstance().getReference();
+    public static StorageReference REF_PHOTOS = REF_STORAGE_ROOT.child(KEY_PHOTOS);
+
+    public static final String MAX_MSG_LENGTH_KEY = "max_msg_length";
+
 }
